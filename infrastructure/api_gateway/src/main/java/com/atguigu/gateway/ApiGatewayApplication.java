4d886a9f2d7d63d09ba16b011b92e18fde145254
CodeHunter_qcy <1715464388@qq.com>
2020-05-28 19:50:29 +0800

    wu

---
+++ infrastructure/api_gateway/src/main/java/com/atguigu/gateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author CodeHunter_qcy
+ * @date 2020/5/28 - 17:50
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class);
+    }
+}
